0516dd2f794ffdb3ebe2779b798cf0dc5b308e4e
Zac Shakespear <zshakespear@gmail.com>
2022-03-20 16:25:56 -0600

    implemented items

--- NPCGen.py
+++ NPCGen.py
@@ -7,10 +7,10 @@ The purpose of this program is to assist in generating
 NPCs for the CDS setting
 
 TO-DO:
-    -Create random NPCs which have a random physical appearance, 
-    a quirk, and a motivation without respect to species
-    -Add unique to species
-    -Split Appearance Tab into physical appearance and items tabs
+    -Let the NPC have a motivation from any one of the columns
+    -Let an NPC have a quirk from any one of the columns
+    -Implement items
+    -Add appearances unique to species
     -Allow user to select varying levels of randomness
     
 """
@@ -26,19 +26,24 @@ Agen = pd.read_excel(wd, sheet_name='Appearance')
 Qgen = pd.read_excel(wd, sheet_name='Quirks')
 Qgen = Qgen[Qgen['Mannerisms'].notna()]
 Mgen= pd.read_excel(wd, sheet_name='Motivations')
+Igen = pd.read_excel(wd, sheet_name='Items&Clothing')
 Arandlimit = Agen['General'].size - 1
 Qrandlimit = Qgen['Mannerisms'].size - 1
 Mrandlimit = Mgen['Relationships'].size - 1
+Irandlimit = Igen.size - 1
 
 npcA = pd.DataFrame([['Appearance: ', Agen['General'].at[random.randint(0,Arandlimit)]]])
+npcI = pd.DataFrame([['Items: ', Igen['General'].at[random.randint(0,Irandlimit)]]])
 npcQ = pd.DataFrame([['Quirk: ', Qgen['Mannerisms'].at[random.randint(0,Qrandlimit)]]])
 npcM = pd.DataFrame([['Motivation: ', Mgen['Relationships'].at[random.randint(0,Mrandlimit)]]])
 
-npcProf = pd.concat([npcA,npcQ,npcM])
+
+npcProf = pd.concat([npcA,npcI,npcQ,npcM])
 
 with open("newNPC.txt", 'w') as file:
         textDummy = npcProf.to_string(header=False, index = False)
         file.write(textDummy)
+
 
 """
 period = input("Enter the number of years for the timeline: ")
